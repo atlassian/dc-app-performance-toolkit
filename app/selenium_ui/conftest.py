8b639a6f81cfa82a21f0459f2515650b23d9996f
Andriy Stashyshyn <astash@softserveinc.com>
2019-10-11 18:06:43 +0300

    DCA-198: Refactor Confluence code to align with new folders structure

--- app/selenium_ui/conftest.py
+++ app/selenium_ui/conftest.py
@@ -1,7 +1,6 @@
 import atexit
 import csv
 import datetime
-import glob
 import json
 import os
 import random
@@ -15,8 +14,8 @@ import yaml
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 
-from util.project_paths import JIRA_YML, JIRA_DATASET_ISSUES, JIRA_DATASET_USERS, JIRA_DATASET_JQLS, \
-    JIRA_DATASET_SCRUM_BOARDS, JIRA_DATASET_KANBAN_BOARDS
+from util.project_paths import JIRA_DATASET_ISSUES, JIRA_DATASET_USERS, JIRA_DATASET_JQLS, \
+    JIRA_DATASET_SCRUM_BOARDS, JIRA_DATASET_KANBAN_BOARDS, JIRA_YML, CONFLUENCE_YML
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
@@ -24,20 +23,23 @@ SCREEN_HEIGHT = 1080
 JTL_HEADER = "timeStamp,elapsed,label,responseCode,responseMessage,threadName,success,bytes,grpThreads,allThreads," \
              "Latency,Hostname,Connect\n"
 
-# create selenium output files
-try:
-    # TODO consider using TAURUS_ARTIFACTS_DIR env variable
-    latest_results_dir = max(glob.glob((str((Path("results") / "jira").absolute()) + '/20*')), key=os.path.getmtime)
-except ValueError:
-    # TODO we have error here if 'results' dir does not exist
-    results_dir_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    pytest_run_results = f'results/{results_dir_name}_local'
-    os.mkdir(pytest_run_results)
-    latest_results_dir = pytest_run_results  # in case you just run pytest
 
-selenium_results_file = Path(latest_results_dir + '/selenium.jtl')
-selenium_error_file = Path(latest_results_dir + '/selenium.err')
-w3c_timings_file = Path(latest_results_dir + '/w3c_timings.txt')
+def __get_current_results_dir():
+    if 'TAURUS_ARTIFACTS_DIR' in os.environ:
+        return os.environ.get('TAURUS_ARTIFACTS_DIR')
+    else:
+        # TODO we have error here if 'results' dir does not exist
+        results_dir_name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        pytest_run_results = f'results/{results_dir_name}_local'
+        os.mkdir(pytest_run_results)
+        return pytest_run_results  # in case you just run pytest
+
+
+# create selenium output files
+current_results_dir = __get_current_results_dir()
+selenium_results_file = Path(current_results_dir + '/selenium.jtl')
+selenium_error_file = Path(current_results_dir + '/selenium.err')
+w3c_timings_file = Path(current_results_dir + '/w3c_timings.txt')
 
 if not selenium_results_file.exists():
     with open(selenium_results_file, "w") as file:
@@ -46,8 +48,8 @@ if not selenium_results_file.exists():
         pass
 
 
-def application_url():
-    with JIRA_YML.open(mode='r') as fs:
+def application_url(yml_file_path: Path):
+    with yml_file_path.open(mode='r') as fs:
         jira_yaml = yaml.load(fs, Loader=yaml.FullLoader)
         protocol = jira_yaml['settings']['env']['application_protocol']
         hostname = jira_yaml['settings']['env']['application_hostname']
@@ -141,7 +143,8 @@ def pytest_runtest_makereport(item):
 
 
 @pytest.fixture
-def screen_shots(request, webdriver):
+def jira_screen_shots(request, webdriver):
+    # TODO come up with a good solution to make 1 function from jira_screen_shots() and confluence_screen_shots()
     yield
     # request.node is an "item" because we use the default
     # "function" scope
@@ -152,17 +155,38 @@ def screen_shots(request, webdriver):
         with open(selenium_error_file, mode) as err_file:
             err_file.write(f"Action: {action_name}, Error: {error_text}\n")
         print(f"Action: {action_name}, Error: {error_text}\n")
-        os.makedirs(f"{latest_results_dir}/errors_artifacts", exist_ok=True)
-        error_artifact_name = f'{latest_results_dir}/errors_artifacts/{datetime_now(action_name)}'
+        os.makedirs(f"{current_results_dir}/errors_artifacts", exist_ok=True)
+        error_artifact_name = f'{current_results_dir}/errors_artifacts/{datetime_now(action_name)}'
         webdriver.save_screenshot('{}.png'.format(error_artifact_name))
         with open(f'{error_artifact_name}.html', 'wb') as html_file:
             html_file.write(webdriver.page_source.encode('utf-8'))
         webdriver.execute_script("window.onbeforeunload = function() {};")  # to prevent alert window (force get link)
-        webdriver.get(application_url())
+        webdriver.get(application_url(JIRA_YML))
+
+
+@pytest.fixture
+def confluence_screen_shots(request, webdriver):
+    yield
+    # request.node is an "item" because we use the default
+    # "function" scope
+    if request.node.rep_call.failed:
+        mode = "w" if not selenium_error_file.exists() else "a+"
+        action_name = request.node.rep_call.head_line
+        error_text = request.node.rep_call.longreprtext
+        with open(selenium_error_file, mode) as err_file:
+            err_file.write(f"Action: {action_name}, Error: {error_text}\n")
+        print(f"Action: {action_name}, Error: {error_text}\n")
+        os.makedirs(f"{current_results_dir}/errors_artifacts", exist_ok=True)
+        error_artifact_name = f'{current_results_dir}/errors_artifacts/{datetime_now(action_name)}'
+        webdriver.save_screenshot('{}.png'.format(error_artifact_name))
+        with open(f'{error_artifact_name}.html', 'wb') as html_file:
+            html_file.write(webdriver.page_source.encode('utf-8'))
+        webdriver.execute_script("window.onbeforeunload = function() {};")  # to prevent alert window (force get link)
+        webdriver.get(application_url(CONFLUENCE_YML))
 
 
 @pytest.fixture(scope="module")
-def datasets():
+def jira_datasets():
     # TODO consider extract common logic with globals to separate function
     global data_sets
     if 'data_sets' in globals():
@@ -178,6 +202,19 @@ def datasets():
         data_sets["kanban_boards"] = __read_input_file(JIRA_DATASET_KANBAN_BOARDS)
 
         return data_sets
+
+
+@pytest.fixture(scope="module")
+def confluence_datasets():
+    # TODO extract paths to project_paths
+    datasets = dict()
+    input_data_path = Path(__file__).parents[1] / "datasets" / "confluence"
+
+    datasets["pages"] = __read_input_file(input_data_path / "pages.csv")
+    datasets["blogs"] = __read_input_file(input_data_path / "blogs.csv")
+    datasets["users"] = __read_input_file(input_data_path / "users.csv")
+
+    return datasets
 
 
 def __read_input_file(file_path):
