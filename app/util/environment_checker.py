814e296ac9af7b2a74c8de92096784b4458e22d3
Oleksandr Metelytsia <ometelytsia@atlassian.com>
2020-01-20 15:03:33 +0200

    add version print

--- app/util/environment_checker.py
+++ app/util/environment_checker.py
@@ -1,6 +1,11 @@
 from sys import version_info
 
+from util.conf import TOOLKIT_VERSION
+
 SUPPORTED_PYTHON_VERSION = (3, 6)
+
+
+print("Data Center App Performance Toolkit version: {}".format(TOOLKIT_VERSION))
 
 python_version = version_info[0:2]
 if python_version < SUPPORTED_PYTHON_VERSION:
