108259d6e11267660ec3416fbc2b5c8a76bcb8ad
Oleksandr Metelytsia <ometelytsia@atlassian.com>
2020-01-20 15:34:42 +0200

    better python version

--- app/util/environment_checker.py
+++ app/util/environment_checker.py
@@ -2,13 +2,14 @@ from sys import version_info
 
 from util.conf import TOOLKIT_VERSION
 
-SUPPORTED_PYTHON_VERSION = (3, 6)
+MIN_SUPPORTED_PYTHON_VERSION = (3, 6, 0)
 
 
 print("Data Center App Performance Toolkit version: {}".format(TOOLKIT_VERSION))
 
-python_version = version_info[0:2]
-if python_version < SUPPORTED_PYTHON_VERSION:
+python_version = version_info[0:3]
+print("Python version: {}".format(python_version))
+if python_version < MIN_SUPPORTED_PYTHON_VERSION:
     raise Exception(
         "Python version {} is not supported. "
-        "Please use Python version {} or higher.".format(python_version, SUPPORTED_PYTHON_VERSION))
+        "Please use Python version {} or higher.".format(python_version, MIN_SUPPORTED_PYTHON_VERSION))
