29048788f147d364e105f7e85605d4100fd8686f
Dag Rende <dag.rende@find-out.se>
2020-01-30 11:28:50 +0100

    changed login to use sessions and automatic resending of JSESSIONID

--- app/pytests/test_login.py
+++ app/pytests/test_login.py
@@ -2,15 +2,14 @@ import requests
 
 class TestLogin:
     def test_login(self):
+        session = requests.session()
+
         # authenticate and get a session id
-        auth_response = requests.post('http://localhost:8080/rest/auth/1/session',
+        auth_response = session.post('http://localhost:8080/rest/auth/1/session',
             json={ "username": "admin", "password": "admin" })
         assert auth_response.status_code == 200
-        session_id = auth_response.json()['session']['value']
 
         # request list of diagrams using the session id
-        diagrams_response = requests.get('http://localhost:8080/rest/dependency-map/1.0/diagram?searchTerm=&startAt=0&maxResults=50',
-            cookies=dict(JSESSIONID=session_id))
+        diagrams_response = session.get('http://localhost:8080/rest/dependency-map/1.0/diagram?searchTerm=&startAt=0&maxResults=50')
         assert diagrams_response.status_code == 200
-        print(diagrams_response.text)
         assert diagrams_response.json()["total"] >= 0
