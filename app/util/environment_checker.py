46ed9fe25a63d06d352b24e75755553fe2d7dce7
Oleksandr Metelytsia <ometelytsia@atlassian.com>
2020-03-10 17:45:56 +0200

    refactoring

--- app/util/environment_checker.py
+++ app/util/environment_checker.py
@@ -1,13 +1,6 @@
-import errno
 from sys import version_info
-import subprocess
-
-from util.conf import TOOLKIT_VERSION
 
 MIN_SUPPORTED_PYTHON_VERSION = (3, 6, 0)
-
-
-print("Data Center App Performance Toolkit version: {}".format(TOOLKIT_VERSION))
 
 python_version = version_info[0:3]
 print("Python version: {}".format(python_version))
@@ -16,8 +9,8 @@ if python_version < MIN_SUPPORTED_PYTHON_VERSION:
         "Python version {} is not supported. "
         "Please use Python version {} or higher.".format(python_version, MIN_SUPPORTED_PYTHON_VERSION))
 
-try:
-    subprocess.call("git")
-except OSError as e:
-    if e.errno == errno.ENOENT:
-      raise Exception("Git is not installed")
+
+# Print toolkit version after Python check
+from util.conf import TOOLKIT_VERSION
+
+print("Data Center App Performance Toolkit version: {}".format(TOOLKIT_VERSION))
