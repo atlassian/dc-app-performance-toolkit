90435dd96e35e29b9aa6ebbd08362ea997953744
Serhii Moroz <smoroz@atlassian.com>
2020-04-13 16:12:18 +0300

    remove insecure from headers

--- app/util/data_preparation/api/abstract_clients.py
+++ app/util/data_preparation/api/abstract_clients.py
@@ -30,7 +30,6 @@ class RestClient(Client):
         "Content-Type": "application/json"
     }
     LOGIN_POST_HEADERS = {
-            'Upgrade-Insecure-Requests': '1',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
                       'application/signed-exchange;v=b3;q=0.9'
