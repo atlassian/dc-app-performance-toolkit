4242ccd5f9732b39a31ee64ae92a574bbf786ba6
Serhii Moroz <smoroz@atlassian.com>
2020-05-24 14:06:00 +0300

    start with locustfile

---
+++ app/locustio/confluence/http_actions.py
@@ -0,0 +1,6 @@
+import itertools
+import inspect
+import logging
+import random
+import re
+
---
+++ app/locustio/confluence/locustfile.py
@@ -0,0 +1,22 @@
+from locust import HttpLocust, TaskSet, task, between
+from locustio.confluence.http_actions import *
+from locustio.common_utils import *
+
+init_logger()
+
+
+class ConfluenceBehavior(TaskSet):
+
+    def on_start(self):
+        login_and_view_dashboard(self)
+
+
+    # @task(1)
+    # def custom_action(self):
+    #     custom_action(self)
+
+
+class JiraUser(HttpLocust):
+    host = JIRA_SETTINGS.server_url
+    task_set = ConfluenceBehavior
+    wait_time = between(0, 0)
