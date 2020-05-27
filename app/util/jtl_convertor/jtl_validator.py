66ebf8d6fb324ed93776765c1416bf7be9727724
Serhii Moroz <smoroz@atlassian.com>
2020-05-27 11:53:29 +0300

    added columns reordering

--- app/util/jtl_convertor/jtl_validator.py
+++ app/util/jtl_convertor/jtl_validator.py
@@ -1,5 +1,6 @@
 import time
-from csv import DictReader
+
+from csv import DictReader, DictWriter
 from pathlib import Path
 from types import FunctionType
 from typing import List, Dict
@@ -20,9 +21,16 @@ RESPONSE_CODE = 'responseCode'
 LABEL = 'label'
 ELAPSED = 'elapsed'
 TIME_STAMP = 'timeStamp'
+METHOD = 'method'
 
 SUPPORTED_JTL_HEADER: List[str] = [TIME_STAMP, ELAPSED, LABEL, RESPONSE_CODE, RESPONSE_MESSAGE, THREAD_NAME,
                                    SUCCESS, BYTES, GRP_THREADS, ALL_THREADS, LATENCY, HOSTNAME, CONNECT]
+# Workaround for https://github.com/Blazemeter/taurus/pull/1311
+OLD_LOCUST_JTL_HEADER: List[str] = [TIME_STAMP, LABEL, METHOD, ELAPSED, BYTES, RESPONSE_CODE, RESPONSE_MESSAGE, SUCCESS,
+                                    ALL_THREADS, LATENCY]
+
+CORRECT_LOCUST_HEADER: List[str] = [TIME_STAMP, ELAPSED, LABEL, RESPONSE_CODE, RESPONSE_MESSAGE, SUCCESS, BYTES,
+                                    GRP_THREADS, ALL_THREADS, LATENCY]
 
 JTL_HEADERS_DIFF_LOCUST_JMETER: List[str] = [THREAD_NAME, CONNECT, HOSTNAME]
 
@@ -85,10 +93,32 @@ def __validate_rows(reader) -> None:
             __raise_validation_error(f"File row number: {file_row_num}. {str(e)}")
 
 
+# https://github.com/Blazemeter/taurus/pull/1311
+def reorder_locust_jtl_header(file_path):
+    updated_row = []
+    with file_path.open(mode='r') as kpi:
+        reader = DictReader(kpi)
+        if reader.fieldnames == OLD_LOCUST_JTL_HEADER:
+            print('Reordering columns for locust kpi.jtl')
+        else:
+            return
+        for row in reader:
+            del row[METHOD]
+            row[GRP_THREADS] = row[ALL_THREADS]
+            updated_row.append(row)
+    with file_path.open(mode='w') as out_kpi:
+        writer = DictWriter(out_kpi, fieldnames=CORRECT_LOCUST_HEADER)
+        writer.writeheader()
+        for row in updated_row:
+            writer.writerow(row)
+    return writer
+
+
 def validate(file_path: Path) -> None:
     print(f'Started validating jtl file: {file_path}')
     start_time = time.time()
-
+    if file_path.name == 'kpi.jtl':
+        reorder_locust_jtl_header(file_path)
     try:
         with file_path.open(mode='r') as f:
             reader: DictReader = DictReader(f)
