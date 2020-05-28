2af1a1eac5653b768e97a6dccf0e940ca4e603dd
mmizin <30836532+mmizin@users.noreply.github.com>
2020-05-28 18:19:02 +0300

    Update populate_db.sh

--- app/util/jira/populate_db.sh
+++ app/util/jira/populate_db.sh
@@ -172,7 +172,7 @@ if [[ ${FREE_SPACE_GB} -lt ${REQUIRED_SPACE_GB} ]]; then
   echo "Required space: ${REQUIRED_SPACE_GB} GB"
   exit 1
 fi
- use computer style progress bar
+#use computer style progress bar
 time wget --progress=dot:giga ${DB_DUMP_URL}
 if [[ $? -ne 0 ]]; then
   echo "DB dump download failed! Pls check available disk space."
