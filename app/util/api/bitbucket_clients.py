5409f5dee0220156da2328a35cc6bc0ace279528
mmizin <30836532+mmizin@users.noreply.github.com>
2020-08-17 13:21:02 +0300

    Update bitbucket_clients.py

--- app/util/api/bitbucket_clients.py
+++ app/util/api/bitbucket_clients.py
@@ -152,7 +152,7 @@ class BitbucketRestClient(RestClient):
 
     def get_locale(self):
         language = None
-        page = self.get(f'{self.host}/dashboard', "Could not get page content.", allow_redirect=True).content
+        page = self.get(f'{self.host}/dashboard', "Could not get page content.").content
         tree = html.fromstring(page)
         try:
             language = tree.xpath('//html/@lang')[0]
