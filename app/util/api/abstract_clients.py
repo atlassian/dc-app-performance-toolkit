b2ff51de4489631cb444c05fd8773383cd5feb5a
Serhii Moroz <smoroz@atlassian.com>
2020-08-17 11:55:39 +0300

    fix redirect for non-rest calls

--- app/util/api/abstract_clients.py
+++ app/util/api/abstract_clients.py
@@ -57,24 +57,24 @@ class RestClient(Client):
     def base_auth(self):
         return self.user, self.password
 
-    def get(self, url: str, error_msg: str, expected_status_codes: list = None):
+    def get(self, url: str, error_msg: str, expected_status_codes: list = None, allow_redirect=False):
         response = self.session.get(url, auth=self.base_auth, verify=False, timeout=self.requests_timeout,
-                                    allow_redirects=False)
+                                    allow_redirects=allow_redirect)
         self.__verify_response(response, error_msg, expected_status_codes)
         return response
 
-    def post(self, url: str, error_msg: str, body: dict = None, params=None):
+    def post(self, url: str, error_msg: str, body: dict = None, params=None, allow_redirect=False):
         body_data = self.to_json(body) if body else None
         response = self.session.post(url, body_data, params=params, auth=self.base_auth, headers=self.JSON_HEADERS,
-                                     allow_redirects=False)
+                                     allow_redirects=allow_redirect)
 
         self.__verify_response(response, error_msg)
         return response
 
-    def put(self, url: str, error_msg: str, body: dict = None, params=None):
+    def put(self, url: str, error_msg: str, body: dict = None, params=None, allow_redirect=False):
         body_data = self.to_json(body) if body else None
         response = self.session.put(url, body_data, params=params, auth=self.base_auth, headers=self.JSON_HEADERS,
-                                    allow_redirects=False)
+                                    allow_redirects=allow_redirect)
 
         self.__verify_response(response, error_msg)
         return response
