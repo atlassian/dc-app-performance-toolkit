8d0d838a98f76ffaaf27c9ce4ff0b2d50c527e9f
Serhii Moroz <smoroz@atlassian.com>
2020-12-01 08:37:33 +0200

    added example for custom actions

--- app/selenium_ui/jsm_ui_agents.py
+++ app/selenium_ui/jsm_ui_agents.py
@@ -1,6 +1,6 @@
 from selenium_ui.jsm import modules_agents
 import pytest
-from extension.jira import extension_ui  # TODO develop example of app-specific action for jsm
+from extension.jsm import extension_ui_agents
 
 
 def is_dataset_small(jsm_datasets):
@@ -73,8 +73,8 @@ Add custom actions anywhere between login and log out action. Move this to a dif
 Write your custom selenium scripts in `app/extension/jsm/extension_ui.py`.
 Refer to `app/selenium_ui/jsm/modules_customers.py` for examples.
 """
-# def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+# def test_1_selenium_custom_action(jsm_webdriver, jsm_datasets, jsm_screen_shots):
+#     extension_ui_agents.app_specific_action(jsm_webdriver, jsm_datasets)
 
 
 # this action should be the last one
--- app/selenium_ui/jsm_ui_customers.py
+++ app/selenium_ui/jsm_ui_customers.py
@@ -1,5 +1,5 @@
 from selenium_ui.jsm import modules_customers
-from extension.jira import extension_ui  # TODO develop example of app-specific action for jsm
+from extension.jsm import extension_ui_customers
 
 
 # this action should be the first one
@@ -36,8 +36,8 @@ Add custom actions anywhere between login and log out action. Move this to a dif
 Write your custom selenium scripts in `app/extension/jsm/extension_ui.py`.
 Refer to `app/selenium_ui/jsm/modules_customers.py` for examples.
 """
-# def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+# def test_1_selenium_custom_action(jsm_webdriver, jsm_datasets, jsm_screen_shots):
+#     extension_ui_customers.app_specific_action(jsm_webdriver, jsm_datasets)
 
 
 # this action should be the last one
