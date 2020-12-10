624aa7bc479b69a6b02ab256d73389c3b1ef2f3a
Stefan Ernst <ernst.stefan@icloud.com>
2020-12-10 17:55:03 +0100

    back with ES

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -8,12 +8,12 @@ from selenium_ui.base_page import BasePage
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
 
-    @print_timing("selenium_app_custom_action2")
+    @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action2:personal_dashboard")
+        @print_timing("selenium_app_custom_action:shared_pages")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/enhancedDashboards/overview.action")
-            page.wait_until_visible((By.ID, "app"))  # Wait for title field visible
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/sharedPages/shared.action")
+            page.wait_until_visible((By.ID, "overviewTable"))  # Wait for overview visible
         sub_measure()
     measure()
--- app/selenium_ui/confluence_ui.py
+++ app/selenium_ui/confluence_ui.py
@@ -39,14 +39,14 @@ Refer to `app/selenium_ui/confluence/modules.py` for examples.
 # def test_1_selenium_custom_action(confluence_webdriver, confluence_datasets, confluence_screen_shots):
 #     extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
 
-# def test_1_selenium_custom_action_shared_pages(confluence_webdriver, confluence_datasets, confluence_screen_shots):
-#     extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
+def test_1_selenium_custom_action_shared_pages(confluence_webdriver, confluence_datasets, confluence_screen_shots):
+    extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
 
 # def test_1_selenium_custom_action_read_confirmations(confluence_webdriver, confluence_datasets, confluence_screen_shots):
 #     extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
 
-def test_1_selenium_custom_action_dashboard_manager(confluence_webdriver, confluence_datasets, confluence_screen_shots):
-    extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
+# def test_1_selenium_custom_action_dashboard_manager(confluence_webdriver, confluence_datasets, confluence_screen_shots):
+#     extension_ui.app_specific_action(confluence_webdriver, confluence_datasets)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(confluence_webdriver, confluence_datasets, confluence_screen_shots):
