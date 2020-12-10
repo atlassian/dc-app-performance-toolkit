23c85adeaa97beda45781dbe36cd9c9b8f970095
Stefan Ernst <ernst.stefan@icloud.com>
2020-12-10 11:21:40 +0100

    Enhanced Sharing Changes

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -7,17 +7,13 @@ from selenium_ui.base_page import BasePage
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page = datasets['custom_pages']
-        app_specific_page_id = app_specific_page[0]
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_app_custom_action:shared_pages")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/plugins/sharedPages/shared.action")
+            page.wait_until_visible((By.ID, "overviewTable"))  # Wait for overview visible
         sub_measure()
     measure()
