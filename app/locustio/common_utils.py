b4416d33149e24b9d8a3d5bd4de0b738da0e84d3
mmizin <mmizin@atlassian.com>
2020-12-23 15:16:43 +0200

    NONE:Update locust action names for confluence

--- app/locustio/common_utils.py
+++ app/locustio/common_utils.py
@@ -1,3 +1,5 @@
+import functools
+
 from locust import events
 import time
 import csv
@@ -159,21 +161,25 @@ def jsm_customer_measure(func):
     return wrapper
 
 
-def confluence_measure(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = global_measure(func, start, *args, **kwargs)
+def confluence_measure(interaction=None):
+    assert interaction is not None, "Interaction name is not passed to the confluence_measure decorator"
 
-        total = (time.time() - start)
-        if total < confluence_action_time:
-            sleep = (confluence_action_time - total)
-            logger.info(f'action: {func.__name__}, action_execution_time: {total}, sleep {sleep}')
-            time.sleep(sleep)
-        return result
-    return wrapper
+    def deco_wrapper(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = global_measure(func, start, interaction, *args, **kwargs)
+            total = (time.time() - start)
+            if total < confluence_action_time:
+                sleep = (confluence_action_time - total)
+                logger.info(f'action: {interaction}, action_execution_time: {total}, sleep {sleep}')
+                time.sleep(sleep)
+            return result
+        return wrapper
+    return deco_wrapper
 
 
-def global_measure(func, start_time, *args, **kwargs):
+def global_measure(func, start_time, interaction, *args, **kwargs):
     result = None
     try:
         result = func(*args, **kwargs)
@@ -181,18 +187,18 @@ def global_measure(func, start_time, *args, **kwargs):
         total = int((time.time() - start_time) * 1000)
         print(e)
         events.request_failure.fire(request_type="Action",
-                                    name=f"locust_{func.__name__}",
+                                    name=interaction,
                                     response_time=total,
                                     response_length=0,
                                     exception=e)
-        logger.error(f'{func.__name__} action failed. Reason: {e}')
+        logger.error(f'{interaction} action failed. Reason: {e}')
     else:
         total = int((time.time() - start_time) * 1000)
         events.request_success.fire(request_type="Action",
-                                    name=f"locust_{func.__name__}",
+                                    name=interaction,
                                     response_time=total,
                                     response_length=0)
-        logger.info(f'{func.__name__} is finished successfully')
+        logger.info(f'{interaction} is finished successfully')
     return result
 
 
