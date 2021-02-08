8f07d2c67bde4c7507abf9ef418390fe7279d02a
mmizin <mmizin@atlassian.com>
2021-02-08 18:47:07 +0200

    [JSM] DCA-1011: Selenium stability (#511)
    * DCA-1011:Fixed selenium_agent_view_queues_medium, selenium_agent_view_queues_small actions; Update fix for agent add_comment action
    
    * DCA-1011:Fix add comment action for agents
    
    * DCA-1011:Update fix for agent add comment action
    
    * DCA-1011:Refactor timeout variable for wait* methods

--- app/selenium_ui/jsm/pages/agent_pages.py
+++ app/selenium_ui/jsm/pages/agent_pages.py
@@ -75,22 +75,19 @@ class ViewCustomerRequest(BasePage):
 
     def add_request_comment(self, rte_status):
         comment_text = f"Add comment from selenium - {self.generate_random_string(30)}"
-        self.get_element(ViewCustomerRequestLocators.comment_collapsed_textarea).click()
+        textarea = self.get_element(ViewCustomerRequestLocators.comment_collapsed_textarea)
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", textarea)
+        textarea.click()
 
         if rte_status:
             self.wait_until_available_to_switch(ViewCustomerRequestLocators.comment_text_field_RTE)
-            if self.driver.app_settings.secure:
-                self.get_element(ViewCustomerRequestLocators.comment_tinymce_field).send_keys(comment_text)
-            else:
-                self.action_chains().move_to_element(self.get_element(ViewCustomerRequestLocators.comment_tinymce_field)
-                                                     ).send_keys(comment_text).perform()
+            tinymce_field = self.get_element(ViewCustomerRequestLocators.comment_tinymce_field)
+            self.action_chains().send_keys_to_element(tinymce_field, comment_text).perform()
             self.return_to_parent_frame()
         else:
-            if self.driver.app_settings.secure:
-                self.get_element(ViewCustomerRequestLocators.comment_text_field).send_keys(comment_text)
-            else:
-                self.action_chains().move_to_element(self.get_element(ViewCustomerRequestLocators.comment_text_field)
-                                                     ).send_keys(comment_text).perform()
+            comment_text_field = self.get_element(ViewCustomerRequestLocators.comment_text_field)
+            self.action_chains().move_to_element(comment_text_field).click()\
+                .send_keys_to_element(comment_text_field, comment_text).perform()
 
         self.get_element(ViewCustomerRequestLocators.comment_internally_btn).click()
         self.wait_until_visible(ViewCustomerRequestLocators.comment_collapsed_textarea)
@@ -150,7 +147,7 @@ class ViewQueue(BasePage):
         self.page_url = url_manager.view_queue_all_open()
 
     def wait_for_page_loaded(self):
-        self.wait_until_clickable(ViewQueueLocators.reporter)
+        self.wait_until_visible(ViewQueueLocators.queues_status)
 
     def get_random_queue(self):
         queues = self.get_elements(ViewQueueLocators.queues)
@@ -159,4 +156,4 @@ class ViewQueue(BasePage):
                                       ['All open', 'Recently resolved', 'Resolved past 7 days']
                                       and queue.text.partition('\n')[2] != '0'])
         random_queue.click()
-        self.wait_until_clickable(ViewQueueLocators.reporter)
+        self.wait_until_visible(ViewQueueLocators.queues_status, timeout=self.timeout)
