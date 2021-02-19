0a503e474dc9718d7bb33799382caaf3559f5a65
Oleksandr Metelytsia <ometelytsia@atlassian.com>
2021-02-19 14:54:09 +0200

    DCA-1023 auto-zipping run results folders

--- app/reports_generation/csv_chart_generator.py
+++ app/reports_generation/csv_chart_generator.py
@@ -1,7 +1,7 @@
 import datetime
 from pathlib import Path
 
-from scripts import config_provider, csv_aggregator, chart_generator, summary_aggregator
+from scripts import config_provider, csv_aggregator, chart_generator, summary_aggregator, results_archivator
 
 
 def main():
@@ -12,6 +12,7 @@ def main():
     chart_generator_config = config_provider.get_chart_generator_config(config, agg_csv)
     chart_generator.perform_chart_creation(chart_generator_config, results_dir)
     summary_aggregator.aggregate(config, results_dir)
+    results_archivator.archive_results(config, results_dir)
 
 
 def __get_results_dir() -> Path:
