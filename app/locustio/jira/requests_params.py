904142f79f6a595eabe6ed79d7aec704919c69f4
mmizin <mmizin@atlassian.com>
2021-04-28 17:14:05 +0300

    DCA-1106:Finished updating actions up to view_project_summary

--- app/locustio/jira/requests_params.py
+++ app/locustio/jira/requests_params.py
@@ -38,8 +38,8 @@ class Login(JiraResource):
     }
 
 
-
 class BrowseIssue(JiraResource):
+    action_name = "view_issue"
     issue_id_pattern = r'id="key-val" rel="(.+?)">'
     project_avatar_id_pattern = r'projectavatar\?avatarId\=(.+?)" '
     edit_allow_pattern = "secure\/EditLabels\!default"  # noqa W605
@@ -51,6 +51,7 @@ class ViewDashboard(JiraResource):
 
 
 class CreateIssue(JiraResource):
+    action_name = 'create_issue'
     atl_token_pattern = '"atl_token":"(.+?)"'
     form_token_pattern = '"formToken":"(.+?)"'
     issue_type_pattern = '\{&quot;label&quot;:&quot;Story&quot;,&quot;value&quot;:&quot;([0-9]*)&quot;'  # noqa W605
@@ -58,8 +59,8 @@ class CreateIssue(JiraResource):
     resolution_done_pattern = r'<option value=\\"([0-9]*)\\">\\n            Done\\n'
     fields_to_retain_pattern = '"id":"([a-z]*)","label":"[A-Za-z0-9\- ]*","required":(false|true),'  # noqa W605
     custom_fields_to_retain_pattern = '"id":"customfield_([0-9]*)","label":"[A-Za-z0-9\- ]*","required":(false|true),'  # noqa W605
-    user_preferences_payload = {"useQuickForm": False, "fields": ["summary", "description",
-                                                                  "priority", "versions", "components"],
+    user_preferences_payload = {"useQuickForm": False,
+                                "fields": ["summary", "description", "priority", "versions", "components"],
                                 "showWelcomeScreen": True}
     create_issue_key_pattern = '"issueKey":"(.+?)"'
     err_message_create_issue = 'Issue was not created'
