0dbaa6ef79bda989e892699ed97d3d907694d0be
Hadi Abou Hamzeh <67643814+hadiabouhamze@users.noreply.github.com>
2021-05-06 14:35:44 +0300

    MJMOBC-103

--- app/extension/jsm/extension_ui_agents.py
+++ app/extension/jsm/extension_ui_agents.py
@@ -6,25 +6,6 @@ from selenium_ui.jsm.pages.agent_pages import Login
 from util.conf import JSM_SETTINGS
 
 
-def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
-
-    @print_timing("selenium_agent_app_custom_action")
-    def measure():
-
-        @print_timing("selenium_agent_app_custom_action:view_request")
-        def sub_measure():
-            page.go_to_url(f"{JSM_SETTINGS.server_url}/browse/{issue_key}")
-            # Wait for summary field visible
-            page.wait_until_visible((By.ID, "summary-val"))
-            # Wait for you app-specific UI element by ID selector
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))
-        sub_measure()
-    measure()
-
-
 def app_specific_action_notification_config_web_action_support(webdriver, datasets):
     page = BasePage(webdriver)
 
