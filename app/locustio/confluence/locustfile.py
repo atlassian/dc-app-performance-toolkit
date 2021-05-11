0747798e385f2f1d2073b71fefa0bdf9f230e8b2
mmizin <mmizin@atlassian.com>
2021-05-11 17:19:36 +0300

    Confluence/dca 1105 review locust actions on newer confluence versions (#603)
    * DCA-1105:Update view_blog action
    
    * DCA-1105:Update search cql and view results action
    
    * DCA-1105:Update create blog action
    
    * DCA-1105:Working on create and edit page action
    
    * DCA-1105:Update create and edit page action
    
    * DCA-1105:Prepare actions for CI test
    
    * DCA-1105:Update default actions
    
    * DCA-1105:Rmove redundant 'f' string
    
    * DCA-1105: Remove redundant line

--- app/locustio/confluence/locustfile.py
+++ app/locustio/confluence/locustfile.py
@@ -2,7 +2,7 @@ from locust import HttpUser, task, between
 
 from extension.confluence.extension_locust import app_specific_action
 from locustio.common_utils import LocustConfig, MyBaseTaskSet
-from locustio.confluence.http_actions import login_and_view_dashboard, view_page_and_tree, view_dashboard, view_blog, \
+from locustio.confluence.http_actions import login_and_view_dashboard, view_dashboard, view_blog, \
     search_cql_and_view_results, open_editor_and_create_blog, create_and_edit_page, comment_page, view_attachments, \
     upload_attachments, like_page
 from util.conf import CONFLUENCE_SETTINGS
