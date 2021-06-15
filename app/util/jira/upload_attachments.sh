d46f78c009fa4d8c6697affcdf627a5185143cdb
mmizin <mmizin@atlassian.com>
2021-06-15 11:08:12 +0300

    DCA-1176:Update jsm version 4.5.15

--- app/util/jira/upload_attachments.sh
+++ app/util/jira/upload_attachments.sh
@@ -28,7 +28,7 @@ JIRA_VERSION_FILE="/media/atl/jira/shared/jira-software.version"
 
 # Jira/JSM supported versions
 SUPPORTED_JIRA_VERSIONS=(8.5.12 8.13.7)
-SUPPORTED_JSM_VERSIONS=(4.5.12 4.13.7)
+SUPPORTED_JSM_VERSIONS=(4.5.15 4.13.7)
 
 SUPPORTED_VERSIONS=("${SUPPORTED_JIRA_VERSIONS[@]}")
 if [[ ${jsm} == 1 ]]; then
