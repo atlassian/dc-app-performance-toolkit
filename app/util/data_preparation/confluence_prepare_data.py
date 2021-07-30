fb9b27927a3220c1293a49b3a07160d9340bd966
Oleksandr Popov <opopov@atlassian.com>
2021-07-30 13:57:40 +0300

    DCA-1016: Confluence prepare data script update

--- app/util/data_preparation/confluence_prepare_data.py
+++ app/util/data_preparation/confluence_prepare_data.py
@@ -3,12 +3,15 @@ import string
 
 import urllib3
 
+from multiprocessing import cpu_count
+from multiprocessing.pool import ThreadPool
 from util.conf import CONFLUENCE_SETTINGS
 from util.api.confluence_clients import ConfluenceRpcClient, ConfluenceRestClient
 from util.project_paths import CONFLUENCE_USERS, CONFLUENCE_PAGES, CONFLUENCE_BLOGS, CONFLUENCE_CUSTOM_PAGES
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+NUM_CORES = cpu_count()
 USERS = "users"
 PAGES = "pages"
 CUSTOM_PAGES = "custom_pages"
@@ -21,17 +24,44 @@ ENGLISH_US = 'en_US'
 ENGLISH_GB = 'en_GB'
 
 
+def print_timing(message, sep='-'):
+    assert message is not None, "Message is not passed to print_timing decorator"
+
+    def deco_wrapper(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> object:
+            start = timer()
+            print(sep * 20)
+            print(f'{message} started {datetime.datetime.now().strftime("%H:%M:%S")}')
+            result = func(*args, **kwargs)
+            end = timer()
+            print(f"{message} finished in {timedelta(seconds=end - start)}")
+            print(sep * 20)
+            return result
+
+        return wrapper
+
+    return deco_wrapper
+
+
 def generate_random_string(length=20):
     return "".join([random.choice(string.ascii_lowercase) for _ in range(length)])
 
 
+@print_timing('Creating dataset started')
 def __create_data_set(rest_client, rpc_client):
     dataset = dict()
     dataset[USERS] = __get_users(rest_client, rpc_client, CONFLUENCE_SETTINGS.concurrency)
     perf_user = random.choice(dataset[USERS])['user']
     perf_user_api = ConfluenceRestClient(CONFLUENCE_SETTINGS.server_url, perf_user['username'], DEFAULT_USER_PASSWORD)
-    dataset[PAGES] = __get_pages(perf_user_api, 5000)
-    dataset[BLOGS] = __get_blogs(perf_user_api, 5000)
+
+    def __get_pages_blogs(func):
+        return func(perf_user_api, 5000)
+
+    pool = ThreadPool(processes=2)
+    blogs, pages = pool.map(__get_pages_blogs, [__get_blogs, __get_pages])
+    dataset[PAGES] = pages
+    dataset[BLOGS] = blogs
     dataset[CUSTOM_PAGES] = __get_custom_pages(perf_user_api, 5000, CONFLUENCE_SETTINGS.custom_dataset_query)
     print(f'Users count: {len(dataset[USERS])}')
     print(f'Pages count: {len(dataset[PAGES])}')
@@ -40,7 +70,7 @@ def __create_data_set(rest_client, rpc_client):
     print(f'Custom pages count: {len(dataset[CUSTOM_PAGES])}')
     return dataset
 
-
+@print_timing('Getting users')
 def __get_users(confluence_api, rpc_api, count):
     errors_count = 0
     cur_perf_users = confluence_api.get_users(DEFAULT_USER_PREFIX, count)
@@ -65,6 +95,7 @@ def __get_users(confluence_api, rpc_api, count):
     return cur_perf_users
 
 
+@print_timing('Getting pages')
 def __get_pages(confluence_api, count):
     pages = confluence_api.get_content_search(
         0, count, cql='type=page'
@@ -79,6 +110,7 @@ def __get_pages(confluence_api, count):
     return pages
 
 
+@print_timing('Getting custom pages')
 def __get_custom_pages(confluence_api, count, cql):
     pages = []
     if cql:
@@ -89,6 +121,7 @@ def __get_custom_pages(confluence_api, count, cql):
     return pages
 
 
+@print_timing('Getting blogs')
 def __get_blogs(confluence_api, count):
     blogs = confluence_api.get_content_search(
         0, count, cql='type=blogpost'
@@ -110,6 +143,7 @@ def __write_to_file(file_path, items):
             f.write(f"{item}\n")
 
 
+@print_timing('Started writing data to files')
 def write_test_data_to_files(dataset):
     pages = [f"{page['id']},{page['space']['key']}" for page in dataset[PAGES]]
     __write_to_file(CONFLUENCE_PAGES, pages)
@@ -144,6 +178,7 @@ def __check_for_admin_permissions(confluence_api):
         raise SystemExit(f"The '{confluence_api.user}' user does not have admin permissions.")
 
 
+@print_timing('Confluence data preparation')
 def main():
     print("Started preparing data")
 
