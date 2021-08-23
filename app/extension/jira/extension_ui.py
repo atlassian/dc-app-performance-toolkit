d86d7804ec14c7f98f1480b4d15026fd69f82e20
Cheney Ma <cheney.ma@aliyun.com>
2021-08-23 20:22:54 +0800

    app specific actions are added

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -6,6 +6,8 @@ from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
 from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def app_specific_action(webdriver, datasets):
@@ -13,32 +15,82 @@ def app_specific_action(webdriver, datasets):
     if datasets['custom_issues']:
         issue_key = datasets['custom_issue_key']
 
-    # To run action as specific user uncomment code bellow.
+  # To run action as specific user uncomment code bellow.
     # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
     # just before test_2_selenium_z_log_out action
     #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_specific_user_login")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
+        def app_specific_user_login(username='admin', password='admin'):
+            login_page = Login(webdriver)
+            login_page.delete_all_cookies()
+            login_page.go_to()
+            login_page.set_credentials(username=username, password=password)
+#            if login_page.is_first_login():
+#                login_page.first_login_setup()
+#            if login_page.is_first_login_second_page():
+#                login_page.first_login_second_page_setup()
+#            login_page.wait_for_page_loaded()
+        app_specific_user_login(username='admin', password='admin')
+        print("logging in using app specific user login")
+    measure()
+
+
+
+#    @print_timing("selenium_app_custom_action")
+#    def measure():
+#        @print_timing("selenium_app_custom_action:view_issue")
+#        def sub_measure():
+#            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+#            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
+#            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+#        sub_measure()
+    measure()
+
+def navigate_to_test_suite_page(webdriver, datasets):
+    @print_timing("navigate_to_test_suite_page")
+    def measure():
+#        testSuite_page = TestSuite(webdriver)
+#        synapsert_Tests = (By.ID, "synapse-link-synapsert")
+        @print_timing("selenium_app_custom_action:testSuitePage")
         def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+             webdriver.find_element_by_id("synapse-link-synapsert").click()
+             webdriver.find_element_by_id("com.go2group.jira.plugin.synapse:synapse-test-suites-link_lnk").click()
+             webdriver.find_element_by_id("activeTabMessageDiv").is_displayed()
+        sub_measure()
+    measure()
+
+def navigate_to_traceability_report_page(webdriver):
+    @print_timing("navigate_to_traceability_report_page")
+    def measure():
+        @print_timing("selenium_app_custom_action:app_traceability_page")
+        def sub_measure():
+             webdriver.find_element_by_id("synapse-link-synapsert").click()
+             webdriver.find_element_by_id("com.go2group.jira.plugin.synapse:synapse-traceability-link_lnk").click()
+             webdriver.find_element_by_id("s2id_reqProject").is_displayed()
+        sub_measure()
+    measure()
+
+def navigate_to_app_report_page(webdriver):
+    @print_timing("navigate_to_app_report_page")
+    def measure():
+        @print_timing("selenium_app_custom_action:app report page")
+        def sub_measure():
+             webdriver.find_element_by_id("synapse-link-synapsert").click()
+             webdriver.find_element_by_id("com.go2group.jira.plugin.synapse:synapse-reports-link_lnk").click()
+             element = WebDriverWait(webdriver, 10).until(EC.presence_of_element_located((By.XPATH, "//h1[contains(text(),'SynapseRT Reports')]")))
+             webdriver.find_element_by_xpath("//h1[contains(text(),'SynapseRT Reports')]").is_displayed()
+        sub_measure()
+    measure()
+
+def navigate_to_my_settings_page(webdriver):
+    @print_timing("navigate_to_my_settings_page")
+    def measure():
+        @print_timing("selenium_app_custom_action:my settings page")
+        def sub_measure():
+             webdriver.find_element_by_id("synapse-link-synapsert").click()
+             webdriver.find_element_by_id("synapse-link-subscription_lnk").click()
+             webdriver.find_element_by_xpath("//a[contains(text(),'Preferences')]").is_displayed()
         sub_measure()
     measure()
 
--- app/selenium_ui/jira_ui.py
+++ app/selenium_ui/jira_ui.py
@@ -14,42 +14,32 @@ def test_1_selenium_browse_projects_list(jira_webdriver, jira_datasets, jira_scr
 def test_1_selenium_browse_boards_list(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.browse_boards_list(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_create_issue(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.create_issue(jira_webdriver, jira_datasets)
-
 
 def test_1_selenium_edit_issue(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.edit_issue(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_save_comment(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.save_comment(jira_webdriver, jira_datasets)
-
 
 def test_1_selenium_search_jql(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.search_jql(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_view_backlog_for_scrum_board(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_backlog_for_scrum_board(jira_webdriver, jira_datasets)
-
 
 def test_1_selenium_view_scrum_board(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_scrum_board(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_view_kanban_board(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_kanban_board(jira_webdriver, jira_datasets)
-
 
 def test_1_selenium_view_dashboard(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_dashboard(jira_webdriver, jira_datasets)
 
-
 def test_1_selenium_view_issue(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_issue(jira_webdriver, jira_datasets)
-
 
 def test_1_selenium_view_project_summary(jira_webdriver, jira_datasets, jira_screen_shots):
     modules.view_project_summary(jira_webdriver, jira_datasets)
@@ -60,9 +50,20 @@ Add custom actions anywhere between login and log out action. Move this to a dif
 Write your custom selenium scripts in `app/extension/jira/extension_ui.py`.
 Refer to `app/selenium_ui/jira/modules.py` for examples.
 """
-# def test_1_selenium_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
-#     extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+def test_1_selenium_b_custom_action(jira_webdriver, jira_datasets, jira_screen_shots):
+    extension_ui.app_specific_action(jira_webdriver, jira_datasets)
+  
+def test_1_selenium_c_app_test_suites_page(jira_webdriver, jira_datasets):
+    extension_ui.navigate_to_test_suite_page(jira_webdriver, jira_datasets)
 
+def test_1_selenium_d_app_traceability_page(jira_webdriver):
+    extension_ui.navigate_to_traceability_report_page(jira_webdriver)
+
+def test_1_selenium_e_app_reports_page(jira_webdriver):
+    extension_ui.navigate_to_app_report_page(jira_webdriver)
+
+def test_1_selenium_f_app_my_settings_page(jira_webdriver):
+    extension_ui.navigate_to_my_settings_page(jira_webdriver)
 
 # this action should be the last one
 def test_2_selenium_z_log_out(jira_webdriver, jira_datasets, jira_screen_shots):
