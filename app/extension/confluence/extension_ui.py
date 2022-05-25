04136402465b3c7c27bf46104a515238f82e935a
marcin.muszynski <marcin.muszynski@smartbear.com>
2022-05-25 16:19:09 +0200

    page ids changed

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -34,18 +34,21 @@ def app_specific_action(webdriver, datasets):
 
     @print_timing("selenium_app_custom_action")
     def measure():
-
         @print_timing("selenium_app_custom_action:view_page")
         def sub_measure():
             page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
             page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible(
+                (By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
+
         sub_measure()
+
     measure()
+
 
 def zscale_view_test_cases_by_status_macro_in_page(webdriver, datasets):
     page = BasePage(webdriver)
-    zscale_specific_page_id = datasets['custom_page_id']
+    zscale_specific_page_id = 43450373
 
     @print_timing("zscale_specific_user_login")
     def measure():
@@ -60,17 +63,21 @@ def zscale_view_test_cases_by_status_macro_in_page(webdriver, datasets):
                 login_page.first_user_setup()
             all_updates_page = AllUpdates(webdriver)
             all_updates_page.wait_for_page_loaded()
+
         zscale_specific_user_login(username='admin', password='admin')
+
     measure()
 
     @print_timing("view_test_cases_by_status_action")
     def measure():
         check_zscale_content(page, datasets)
+
     measure()
+
 
 def zscale_view_test_cases_by_project_macro_in_page(webdriver, datasets):
     page = BasePage(webdriver)
-    zscale_specific_page_id = datasets['custom_page_id']
+    zscale_specific_page_id = 43450375
 
     @print_timing("zscale_specific_user_login")
     def measure():
@@ -85,17 +92,21 @@ def zscale_view_test_cases_by_project_macro_in_page(webdriver, datasets):
                 login_page.first_user_setup()
             all_updates_page = AllUpdates(webdriver)
             all_updates_page.wait_for_page_loaded()
+
         zscale_specific_user_login(username='admin', password='admin')
+
     measure()
 
     @print_timing("view_test_cases_by_project_action")
     def measure():
         check_zscale_content(page, datasets)
+
     measure()
+
 
 def zscale_view_test_cases_by_folder_macro_in_page(webdriver, datasets):
     page = BasePage(webdriver)
-    zscale_specific_page_id = datasets['custom_page_id']
+    zscale_specific_page_id = 43450378
 
     @print_timing("zscale_specific_user_login")
     def measure():
@@ -110,25 +121,28 @@ def zscale_view_test_cases_by_folder_macro_in_page(webdriver, datasets):
                 login_page.first_user_setup()
             all_updates_page = AllUpdates(webdriver)
             all_updates_page.wait_for_page_loaded()
+
         zscale_specific_user_login(username='admin', password='admin')
+
     measure()
 
     @print_timing("view_test_cases_by_folder_action")
     def measure():
         check_zscale_content(page, datasets)
+
     measure()
 
 
 def check_zscale_content(page, datasets):
     zscale_specific_page_id = datasets['custom_page_id']
-    
+
     page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={zscale_specific_page_id}")
 
-    if zscale_specific_page_id == 38928401:
-        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-summary-by-status"))  
-    elif zscale_specific_page_id == 38928403:
-        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-created-by-project-list")) 
-    elif zscale_specific_page_id == 38928405:
-        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-created-by-folder-list")) 
+    if zscale_specific_page_id == 43450373:
+        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-summary-by-status"))
+    elif zscale_specific_page_id == 43450375:
+        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-created-by-project-list"))
+    elif zscale_specific_page_id == 43450378:
+        page.wait_until_visible((By.TAG_NAME, "reports-viewer-test-cases-created-by-folder-list"))
     else:
-        page.wait_until_visible((By.TAG_NAME, "macro-view"))  
+        page.wait_until_visible((By.TAG_NAME, "macro-view"))
