5d0ee1e5e409cfb0a74ae469fc6d60f376880317
Oleksandr Popov <opopov@atlassian.com>
2022-07-18 14:53:35 +0300

    Deleted not used import and locators

--- app/selenium_ui/jsm/pages/agent_pages.py
+++ app/selenium_ui/jsm/pages/agent_pages.py
@@ -1,5 +1,3 @@
-import random
-
 from selenium_ui.base_page import BasePage
 from selenium.webdriver.common.keys import Keys
 from selenium_ui.jsm.pages.agent_selectors import LoginPageLocators, PopupLocators, DashboardLocators, LogoutLocators, \
