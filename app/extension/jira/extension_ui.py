a7a3f55a0fbe10abe41da2d604a501f50f7ab243
Rui Rodrigues <>
2022-12-27 15:44:27 +0000

    /XPORTER-4034 jira dc toolkit setup

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -7,42 +7,10 @@ from selenium_ui.conftest import print_timing
 from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
+def app_single_action(webdriver, datasets):
+issue_page = Issue(webdriver, issue_key=datasets['issue_key'])
 
-def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
-
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
-
-    @print_timing("selenium_app_custom_action")
-    def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
-    measure()
-
-    def app_single_action(webdriver, datasets):
+def app_single_action(webdriver, datasets):
     issue_page = Issue(webdriver, issue_key=datasets['issue_key'])
 
     @print_timing("selenium_xporter_single_export_dialog")
@@ -117,4 +85,3 @@ def app_xlsx_current_fields_action(webdriver, datasets):
             search_page.wait_until_visible((By.XPATH, "//a[@class='aui-list-item-link'][text()='XLSX (Current fields)']")).click()
         sub_measure()
     selenium_xlsx_current_fields_action()
-
