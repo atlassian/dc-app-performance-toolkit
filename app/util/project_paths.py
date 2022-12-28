0b131a6694aa971a126bd718132fcbf535fc2654
Rui Rodrigues <>
2022-12-28 10:38:28 +0000

    XPORTER-4034 rollback commit: "bulk and current xlsx fields removed." and add custom-jqls.csv.

--- app/util/project_paths.py
+++ app/util/project_paths.py
@@ -102,6 +102,7 @@ JIRA_DATASET_USERS = __get_jira_dataset('users.csv')
 JIRA_DATASET_ISSUES = __get_jira_dataset('issues.csv')
 JIRA_DATASET_PROJECTS = __get_jira_dataset('projects.csv')
 JIRA_DATASET_CUSTOM_ISSUES = __get_jira_dataset('custom-issues.csv')
+JIRA_DATASET_CUSTOM_JQLS = __get_jira_dataset('custom-jqls.csv')
 
 JSM_YML = __get_jsm_yml()
 JSM_DATASETS = __get_jsm_datasets()
