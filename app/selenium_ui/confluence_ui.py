da861cb274616c08e0d52251ab7b55e60550d02f
Bradley Hyde <bhyde@atlassian.com>
2024-11-01 19:43:05 +1100

    disable search queries for testing purposes

--- app/selenium_ui/confluence_ui.py
+++ app/selenium_ui/confluence_ui.py
@@ -39,8 +39,8 @@ def test_1_selenium_create_inline_comment(confluence_webdriver, confluence_datas
     modules.create_inline_comment(confluence_webdriver, confluence_datasets)
 
 
-def test_1_selenium_cql_search(confluence_webdriver, confluence_datasets, confluence_screen_shots):
-    modules.cql_search(confluence_webdriver, confluence_datasets)
+# def test_1_selenium_cql_search(confluence_webdriver, confluence_datasets, confluence_screen_shots):
+#     modules.cql_search(confluence_webdriver, confluence_datasets)
 
 
 """
