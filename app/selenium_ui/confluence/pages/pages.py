b9e3cf886ca5c55a6a0dbe03f7d5140d782112f0
Bradley Hyde <bhyde@atlassian.com>
2025-04-09 19:11:56 +1000

    Revert "set is_2sv_login to true, oops"
    This reverts commit 95dcf025ef4fdc65dfed165d55f84da1dc851c62.

--- app/selenium_ui/confluence/pages/pages.py
+++ app/selenium_ui/confluence/pages/pages.py
@@ -12,7 +12,7 @@ class Login(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
-        self.is_2sv_login = True
+        self.is_2sv_login = False
 
     def wait_for_page_loaded(self):
         self.wait_until_visible(LoginPageLocators.sidebar)
