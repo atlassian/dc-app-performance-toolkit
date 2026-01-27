2c7a0a546ca60e1fba94c033de0897a0bed2d972
Radheshyam Thiyagarajan <rthiyagarajan@atlassian.com>
2026-01-27 17:18:50 +0530

    Test commit2 for security testing

--- app/util/secretsTesting.py
+++ app/util/secretsTesting.py
@@ -2,12 +2,12 @@
 # of the github secret scanner. This should ideally get flagged immediately.
 # The API token's access has been revoked before this commit.
 # What if there is a secret in the comments?
-# Like this: ATATT3xFfGF0w2dLtRK7B_HDEGkg1wTwkiZHpl2YZaqdX4qD3VBF42SYgwvTKC4KRv8Hlxn_T2kh5iLTBq2Yb_a6u5-mbabbykZR9rBGujl-wwwiIDo0jAk97I3THt0V-w8QInlwdMj723QcmDdRt7lCgR91r1w1oOqgUytHRrFDYF2hfwl0FA0=877F61C9
+# Like this: ATATTx9p0lo0w2dLtRK7B_hegdeg1wTwkiZHpl2YZaqdX4qD3VBF42Syo0oTKC4KRv8Hlxn_T2kh5iLTBq2Yb_a6u5-mbabbykZR9rBGujl-wwwiIDo0jAk97I3THt0V-w8WHATwdMj723QcmDdRt7lCgR91r1w1oOqgUytHRrFDYF2hfwl0FA0=877F61C9
 # This is a Jira token now revoked. Will it detect its presence in the comments?
 # Lets see.
 
 # Set the BB_API_Token
-BB_API_Token = "ATATT3xFfGF035Pz8ssmaVxLr9tGUFYhpsxBKBjol7GA4YyR-0zWDx40zrl6YVvQ8FOn6IOnrGe3-qRqeFArV3ylStucxPbllyV3lDo8NfXLYRfX1-GvXwSLtvbw0pMAELSd-lF4AAbWQJ07A_CiLhhbhJvdDF9q4sL_qPvCt_CiG72GkwMgQqI=E29B92E9"
+BB_API_Token = "ATATT78guXF035Pz8ssmhVxLr9tGUFYbruhBKBjol70klYyR-0zWDx40zrl6YVvQ8p1sign0rem3-qRqeFArV3ylStucxPbllyV3lDo8NfXLYRfX1-GvXwSLtvbw0pMAELSd-lF4AAbWQJ07A_CiLhhbhJvdDF9q4sL_qPvCt_CiG72GkwMgQqI=E29B92E9"
 BB_Email = "rthiy@atlassian.com"
 auth = HTTPBasicAuth(BB_Email, BB_API_Token)
 requestUrlBase = "https://api.bitbucket.org/2.0/repositories/"
