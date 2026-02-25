161fb3f20bd70da9074dfd3e9db5aed7f274eddc
Yifei Zhang <yf_zhang@live.com>
2026-02-25 23:15:53 +1100

    DCDORA-2746: test using a different term for jmeter two words search.

--- app/locustio/confluence/http_actions.py
+++ app/locustio/confluence/http_actions.py
@@ -12,7 +12,7 @@ import uuid
 logger = init_logger(app_type='confluence')
 confluence_dataset = confluence_datasets()
 
-TWO_WORDS_CQL = 'sheep agreement'
+TWO_WORDS_CQL = 'confluence agreement'
 THREE_WORDS_CQL = 'shoulder trip discussion'
 
 
